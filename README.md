a9059056e2f56c7ef3bd5408a60668b2b27340b1
dagonis <ktyers@gmail.com>
2015-01-29 17:24:05 -0700

    Adding code and updating readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # rhino
-A python tool to find reverse dns records for blocks of IPs (Python3.3+)
+A python tool to find reverse dns records for blocks of IPs (Python3.3+).
+
+#Usage
+rhino.py [range] <br /><br />
+Optional Arguments<br />
+-o to write results to a file <br />
+-f to log failures, will be written to the above file if that option is enabled <br />
+
+##Example
+python3 rhino.py 4.2.2.0/30 -o rhino_test.txt -f<br /><br />
+Output:<br />
+4.2.2.0 - none<br />
+4.2.2.1 - a.resolvers.level3.net<br />
+4.2.2.2 - b.resolvers.level3.net<br />
+4.2.2.3 - c.resolvers.level3.net<br />
---
+++ rhino.py
@@ -0,0 +1,40 @@
+import argparse
+import socket
+import sys
+
+try:
+    import ipaddress
+except ImportError:
+    print("Couldn't find ipaddress module. Module requires python3.3+, you are using {}".format(sys.version_info.split(" ")[0]))
+    sys.exit()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("iprange", help="The range you want to find the reverse records for.")
+parser.add_argument("--failures", "-f", help="Use this switch to capture failed lookups", action="store_true")
+parser.add_argument("--outfile", "-o", help="Use this to save output to a file")
+args = parser.parse_args()
+
+try:
+    ips = ipaddress.IPv4Network(args.iprange)
+except:
+    print("{} is not a valid subnet address.".format(args.iprange))
+    sys.exit()
+
+if args.outfile:
+    rhino_out = open(args.outfile, 'wb')
+
+for ip in ips:
+    try:
+        if args.outfile:
+            rhino_out.write("{} - {}\n".format(str(ip), socket.gethostbyaddr(str(ip))[0]).encode())
+        else:
+            print("{} - {}".format(str(ip), socket.gethostbyaddr(str(ip))[0]))
+    except socket.herror as e:
+        if args.failures:
+            if args.outfile:
+                rhino_out.write("{} - none\n".format(str(ip)).encode())
+            else:
+                print("{} - none".format(str(ip)))
+        else:
+            pass
+rhino_out.close()
